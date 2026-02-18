3f6355ea7a8e9655b5eaf2b70a9197b377291b02
Eduardo Pech <eduardopech.web@gmail.com>
2026-02-17 23:18:09 -0600

    feat: update icon ico

--- electron/main.ts
+++ electron/main.ts
@@ -33,7 +33,7 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, '/icons/icon.png'),
+    icon: path.join(process.env.VITE_PUBLIC, '/icons/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
